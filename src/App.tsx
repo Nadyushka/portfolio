47650e752f3d0ccda162e5be9288552c74416e85
Nadyushka <12nadya11@gmail.com>
2023-03-04 21:23:34 +0300

    update portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
 
-
 function App() {
     return (
         <div className="App">
